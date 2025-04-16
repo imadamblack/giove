dfa36eb789259279d35ceb2190742b4d3395cf09
jovanni villa <Jovanni.villa35@gmail.com>
2025-04-15 18:05:25 -0600

    clone 01
    clonacion del repositorio

---
+++ bh-consulting-main/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          1: "#1089e0",
+          2: "#143655",
+          3: "#606565",
+          4: "#43b2bc",
+          5: "#37AC1A",
+          6: "#fef0d5",
+        },
+      },
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1280px",
+      },
+    },
+  },
+  plugins: [],
+};
